74f6873f81249e3252a9ee688c913be290eccfd2
Atharva Kailash Dhamale <atharvadhamale43@gmail.com>
2025-10-27 21:07:45 +0530

    Add files via upload

---
+++ Assignment/Assignment_8/program8_5.c
@@ -0,0 +1,30 @@
+
+//Write a program which accept number from user and display its table in reverse order
+
+#include<stdio.h>
+
+void Table(int iNo)
+{
+    int i=0;
+
+    if(iNo<0)
+    {
+        iNo=-iNo;
+    }
+    
+    for(i=10;i>=1;i--)
+    {
+            printf("%d ",iNo*i);
+    }
+}
+int main()
+{
+    int iValue=0;
+    printf("Enter number :");
+    scanf("%d",&iValue);
+
+    Table(iValue);
+    printf("\n");
+
+    return 0;
+}
