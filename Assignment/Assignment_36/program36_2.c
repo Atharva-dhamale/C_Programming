6694dc9867959c97d07bdf148fa7d10fc303a040
Atharva Kailash Dhamale <atharvadhamale43@gmail.com>
2025-12-02 15:21:57 +0530

    Add files via upload

---
+++ Assignment/Assignment_36/program36_2.c
@@ -0,0 +1,30 @@
+
+//write a program which accept string from user and covert it in upper case
+
+#include<stdio.h>
+
+void struprx(char *str)
+{
+   while(*str!='\0')
+   {
+    if((*str>='a')&&(*str<='z'))
+    {
+        *str=*str-32;
+    }
+    
+    str++;
+   }
+}
+int main()
+{
+    char arr[20];
+
+    printf("Enter string: ");
+    scanf("%[^'\n']s",arr);
+
+    struprx(arr);
+
+    printf("Modified String is %s \n",arr);
+
+    return 0;
+}
