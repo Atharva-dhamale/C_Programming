7b36e8a14874affb5aca26b851c2e52364ba4a8d
Atharva Kailash Dhamale <atharvadhamale43@gmail.com>
2025-12-31 22:17:29 +0530

    Add files via upload

---
+++ Assignment/Assignment_48/program48_4.c
@@ -0,0 +1,118 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE first,int no)
+{
+    PNODE newn=NULL;
+    newn=(PNODE)malloc(sizeof(NODE));
+    newn->data=no;
+    newn->next=NULL;
+
+    if(*first==NULL)
+    {
+        *first=newn;
+    }
+    else
+    {
+        newn->next=*first;
+        *first=newn;
+    }
+}
+
+void InsertLast(PPNODE first,int no)
+{
+    PNODE temp=NULL;
+    PNODE newn=NULL;
+    newn=(PNODE)malloc(sizeof(NODE));
+    newn->data=no;
+    newn->next=NULL;
+
+    if(*first==NULL)
+    {
+        *first=newn;
+    }
+    else
+    {
+       temp=*first;
+       
+       while(temp->next!=NULL)
+       {
+        temp=temp->next;
+       }
+       temp->next=newn;
+    }
+}
+
+
+
+
+void Display(PNODE first)
+{
+
+    
+    while(first!=NULL)
+    {
+        printf("| %d |->",first->data);
+        first=first->next;
+    }
+    printf("NULL\n");
+}
+
+void DisplayEvenPos(PPNODE first)
+{
+    int iCnt=0;
+    iCnt=1;
+    while(*first!=NULL)
+    {
+        if((iCnt%2)==0)
+        {
+            printf("| %d |->",(*first)->data);
+            *first=(*first)->next; 
+        }
+        else
+        {
+            *first=(*first)->next; 
+        }
+        iCnt++;
+        
+        
+        
+    } 
+    printf("NULL\n");
+}
+
+
+
+
+int main()
+{
+    PNODE head=NULL;
+
+   
+
+    InsertFirst(&head,27);
+    InsertFirst(&head,92);
+
+    InsertLast(&head,84);
+    InsertLast(&head,31);
+
+    Display(head);
+    
+    
+    DisplayEvenPos(&head);
+    
+    
+
+    return 0;
+}
