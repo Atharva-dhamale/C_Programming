b0902a25e93b3b5272e06778ace8b0a8ad5c0308
Atharva Kailash Dhamale <atharvadhamale43@gmail.com>
2025-12-31 15:09:19 +0530

    Add files via upload

---
+++ Assignment/Assignment_45/program45_3.c
@@ -0,0 +1,115 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE first,int no)
+{
+    PNODE newn=NULL;
+    newn=(PNODE)malloc(sizeof(NODE));
+    newn->data=no;
+    newn->next=NULL;
+
+    if(*first==NULL)
+    {
+        *first=newn;
+    }
+    else
+    {
+        newn->next=*first;
+        *first=newn;
+    }
+}
+
+void InsertLast(PPNODE first,int no)
+{
+    PNODE temp=NULL;
+    PNODE newn=NULL;
+    newn=(PNODE)malloc(sizeof(NODE));
+    newn->data=no;
+    newn->next=NULL;
+
+    if(*first==NULL)
+    {
+        *first=newn;
+    }
+    else
+    {
+       temp=*first;
+       
+       while(temp->next!=NULL)
+       {
+        temp=temp->next;
+       }
+       temp->next=newn;
+    }
+}
+
+
+
+
+void Display(PNODE first)
+{
+
+    
+    while(first!=NULL)
+    {
+        printf("| %d |->",first->data);
+        first=first->next;
+    }
+    printf("NULL\n");
+}
+
+
+int LastOccur(PNODE first,int no)
+{
+    int iCnt=0;
+    iCnt=1;
+    int iNo=0;
+    
+    while(first!=NULL)
+    {
+        if((first->data)==no)
+        {
+            iNo=iCnt; 
+        }
+        iCnt++;
+        first=first->next;
+    }
+    printf("\n");
+    return iNo;
+    
+}
+
+
+
+int main()
+{
+    PNODE head=NULL;
+    int iRet=0;
+
+    InsertFirst(&head,24);
+    InsertFirst(&head,93);
+
+    InsertLast(&head,31);
+    InsertLast(&head,84);
+
+    Display(head);
+
+    iRet=LastOccur(head,93);
+    
+    printf("Position of Last Occurence is :%d\n",iRet);
+
+    
+
+    return 0;
+}
