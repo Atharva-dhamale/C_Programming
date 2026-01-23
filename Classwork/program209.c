dddaf6eef89b06e994a65252933952ad4e84ae99
Atharva Kailash Dhamale <atharvadhamale43@gmail.com>
2026-01-23 23:04:08 +0530

    Add files via upload

---
+++ Classwork/program209.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+    char Arr[]="Hello";
+    printf("%s\n",Arr);
+    return 0;
+}
---
+++ Classwork/program216.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void Display(char Str[])
+{
+    while(*Str!='\0')
+    {
+
+    printf("%c\n",*Str);
+    Str++;
+    }
+   
+}
+int main()
+{
+    char Arr[50]={'\0'};
+
+    printf("Enter String:");
+    scanf("%[^'\n']s",Arr);
+
+    Display(Arr);
+
+    return 0;
+}
