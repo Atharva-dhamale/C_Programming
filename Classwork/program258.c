82af5b9ebe4c587272de05a221345823e9c84525
Atharva Kailash Dhamale <atharvadhamale43@gmail.com>
2026-03-23 20:47:38 +0530

    Add files via upload

---
+++ Classwork/program258.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void strcatX(char *src,char *dest)
+{
+    while(*dest!='\0')
+    {
+        dest++;
+    }
+    while(*src!='\0')
+    {
+        *dest=*src;
+        src++;
+        dest++;
+    }
+    *dest=*src;
+}
+int main()
+{
+    char Arr[50]={'\0'};
+    char Brr[50]={'\0'};
+
+    printf("Enter Source String: ");
+    scanf("%[^'\n']s",Arr);
+
+    printf("Enter Destination String: ");
+    scanf(" %[^'\n']s",Brr);
+
+    strcatX(Arr,Brr);
+    printf("Concatinated String is :%s\n",Brr);
+    return 0;
+}
